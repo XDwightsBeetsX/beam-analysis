ab11a04c0516596ec2a3597ad0d9f5096343f290
XDwightsBeetsX <gutierrezjohn2000@gmail.com>
2021-02-24 18:35:44 -0600

    echo and trying to find README.txt

--- setup.py
+++ setup.py
@@ -18,26 +18,31 @@ SHORT_DESCR = "Perform engineering analysis on a beam"
 
 # Get CURRENT DIRECTORY
 CURR_DIR = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
+print("[LOG] - DIR FILES FOUND:")
+for f in os.listdir(CURR_DIR):
+    print(f"  [LOG] - {f}")
 
 # Get contents of README file
-README_FILENAME = "readme.md"
+README_FILENAME = "README.md"
 README_PATH = os.path.join(CURR_DIR, README_FILENAME)
-print(f"[LOG] - Searching for '{README_FILENAME}'  in '{CURR_DIR}'")
+
+print(f"[LOG] - Searching for '{README_FILENAME}' in '{CURR_DIR}'")
 with open(README_PATH, "r", encoding="utf-8") as readme:
     LONG_DESCR = readme.read()
-    print(f"[LOG] - Found readme")
+    print(f"[LOG] - Found '{README_FILENAME}'")
 
 # Get contents of REQUIREMENTS file
 REQUIREMENTS = []
-REQUIREMENTS_FILENAME = "requirements.txt"
+REQUIREMENTS_FILENAME = "REQUIREMENTS.txt"
 REQUIREMENTS_PATH = os.path.join(CURR_DIR, REQUIREMENTS_FILENAME)
+
 print(f"[LOG] - Searching for '{REQUIREMENTS_FILENAME}' in '{CURR_DIR}'")
 with open(REQUIREMENTS_PATH, "r", encoding="utf-8") as req:
     lines = req.readlines()
     for line in lines:
         if line[0] != "#" and line.strip() != "":
             REQUIREMENTS.append(line.strip())
-    print(f"[LOG] - Found requirements")
+    print(f"[LOG] - Found '{REQUIREMENTS_FILENAME}'")
 
 # SETUP
 setup(
