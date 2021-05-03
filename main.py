4571752fedf20ab8e3b858ec4a37564224183d7b
John Gutierrez <gutierrezjohn2000@gmail.com>
2021-05-02 20:40:17 -0500

    Update main.py

--- main.py
+++ main.py
@@ -19,22 +19,32 @@ if __name__ == "__main__":
     # =============================== #
     E = 207 * 10**9
     I = 2 * 10**-8
-    L = 1.0
+    
+    a = 2
+    b = 1
+    L = 2*a + b
+    w = 100
+    P = 1000
 
     # =============================== #
     # = Make the beam and add loads = #
     # =============================== #
     B = Beam(L, E, I)
-    B.addPointLoad(0, 11000)
-    B.addPointLoad(1, 11000)
-    B.addPointLoad(0.5, -21000)
-    B.addDistributedLoad(0, 1, -1000)
+    
+    B.addDistributedLoad(0, a, -w)
+    B.addPointLoad(a+b/2, -P)
+    B.addDistributedLoad(a+b, L, -w)
+
+    rb = (1/L)*(w*a*(2*a+b)+P*(a+b/2))
+    ra = rb
+    B.addPointLoad(0, ra)
+    B.addPointLoad(L, rb)
 
     # =============================== #
     # === Add Boundary Conditions === #
     # =============================== #
     # currently req 1 to be angle and other to be deflection
-    B.addBoundaryCondition(0.5, "angle", 0.0)
+    B.addBoundaryCondition(L/2, "angle", 0.0)
     B.addBoundaryCondition(0.0, "deflection", 0.0)
 
     # =============================== #
