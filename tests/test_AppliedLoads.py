1da09ca7e2e14d8606db8b48a859268d82bca921
John Gutierrez <gutierrezjohn2000@gmail.com>
2022-01-01 12:54:31 -0600

    unit tests for appliedLoads. fixed Moment bug for location check

--- tests/test_AppliedLoads.py
+++ tests/test_AppliedLoads.py
@@ -3,28 +3,57 @@ from beam_analysis.BeamAnalysisTypes import BeamAnalysisTypes
 
 
 class Test_PointLoad:
-    def test_PointLoad_Shear(self):
+    def test_Shear_Analysis(self):
         loc = .5
         mag = 10
         pl = PointLoad(loc, mag)
         
+        x = loc
+        result = pl.evaluateAt(x, BeamAnalysisTypes.SHEAR)
         expected = mag
-        result = pl.evaluateAt(.5, BeamAnalysisTypes.SHEAR)
-        
+
         tol = 1E-10
         test = abs(result - expected) < tol
         
         assert test
     
-    def test_PointLoad_Moment(self):
+    def test_Bending_Analysis(self):
         loc = 0
         mag = 10
         pl = PointLoad(loc, mag)
         
         x = 2
-        expected = mag * (x - loc)
         result = pl.evaluateAt(x, BeamAnalysisTypes.BENDING)
+        expected = mag * (x - loc)
+
+        tol = 1E-10
+        test = abs(result - expected) < tol
         
+        assert test
+    
+    def test_Angle_Analysis(self):
+        loc = 0
+        mag = 10
+        pl = PointLoad(loc, mag)
+        
+        x = 2
+        result = pl.evaluateAt(x, BeamAnalysisTypes.ANGLE)
+        expected = (mag / 2) * (x - loc)**2
+
+        tol = 1E-10
+        test = abs(result - expected) < tol
+        
+        assert test
+    
+    def test_Deflection_Analysis(self):
+        loc = 0
+        mag = 10
+        pl = PointLoad(loc, mag)
+        
+        x = 2
+        result = pl.evaluateAt(x, BeamAnalysisTypes.DEFLECTION)
+        expected = (mag / 6) * (x - loc)**3
+
         tol = 1E-10
         test = abs(result - expected) < tol
         
@@ -32,16 +61,113 @@ class Test_PointLoad:
 
 
 class Test_DistributedLoad:
-    def test_DistributedLoad_Shear(self):
-        start, stop = 0, 1
+    def test_Shear_Analysis(self):
+        start, stop = 0, 3
         mag = 5
-        pl = DistributedLoad(start, stop, mag)
+        dl = DistributedLoad(start, stop, mag)
         
         x = 2
-        expected = mag
-        result = pl.evaluateAt(x, BeamAnalysisTypes.SHEAR)
-        
+        result = dl.evaluateAt(x, BeamAnalysisTypes.SHEAR)
+        expected = mag * (x - start)
+
         tol = 1E-10
         test = abs(result - expected) < tol
+        
+        assert test
+    
+    def test_Bending_Analysis(self):
+        start, stop = 0, 3
+        mag = 5
+        dl = DistributedLoad(start, stop, mag)
+        
+        x = 3
+        result = dl.evaluateAt(x, BeamAnalysisTypes.BENDING)
+        expected = (mag / 2) * (x - start)**2
+
+        tol = 1E-10
+        test = abs(result - expected) < tol
+        
+        print(expected, result)
+        assert test
+    
+    def test_Angle_Analysis(self):
+        start, stop = 0, 3
+        mag = 5
+        dl = DistributedLoad(start, stop, mag)
+        
+        x = 2
+        result = dl.evaluateAt(x, BeamAnalysisTypes.ANGLE)
+        expected = (mag / 6) * (x - start)**3
+
+        tol = 1E-10
+        test = abs(result - expected) < tol
+        
+        assert test
+    
+    def test_Deflection_Analysis(self):
+        start, stop = 0, 3
+        mag = 5
+        dl = DistributedLoad(start, stop, mag)
+        
+        x = 2
+        result = dl.evaluateAt(x, BeamAnalysisTypes.DEFLECTION)
+        expected = (mag / 24) * (x - start)**4
+
+        tol = 1E-10
+        test = abs(result - expected) < tol
+        
+        assert test
+
+
+class Test_Moment:
+    def test_Shear_Analysis(self):
+        loc = 0
+        mag = 5
+        m = Moment(loc, mag)
+        
+        x = 2
+        result = m.evaluateAt(x, BeamAnalysisTypes.SHEAR)
+        expected = 0.0
+
+        test = result == expected
+        
+        assert test
+    
+    def test_Bending_Analysis(self):
+        loc = 0
+        mag = 5
+        m = Moment(loc, mag)
+        
+        x = 3
+        result = m.evaluateAt(x, BeamAnalysisTypes.BENDING)
+        expected = mag
+
+        test = result == expected
+        
+        assert test
+
+    def test_Angle_Analysis(self):
+        loc = 0
+        mag = 5
+        m = Moment(loc, mag)
+        
+        x = 3
+        result = m.evaluateAt(x, BeamAnalysisTypes.ANGLE)
+        expected = mag * (x - loc)
+        
+        test = result == expected
+        
+        assert test
+    
+    def test_Deflection_Analysis(self):
+        loc = 0
+        mag = 5
+        m = Moment(loc, mag)
+        
+        x = 3
+        result = m.evaluateAt(x, BeamAnalysisTypes.DEFLECTION)
+        expected = (mag / 2) * (x - loc)**2
+        
+        test = result == expected
         
         assert test
